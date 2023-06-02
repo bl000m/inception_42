5dc9e6d544f36efe5c6571b36b3ca7b48c660678
mpagani <mathiapagani@gmail.com>
2023-06-02 06:37:23 -0400

    finished and clean

--- Makefile
+++ Makefile
@@ -1,64 +1,43 @@
 
 DOCKER_COMPOSE := docker-compose -f srcs/docker-compose.yml
-# -f = specify the location of the compose file
-
-
-# colors
-_BACK			:=	\e[0m]
-_YELLOW			:=	\e[1;32]
 
 all: up
 
 up:
-	@echo "$(_YELLOW) Rebuild and start all the containers in detached mode$(_BACK)"
+	@echo "Rebuild and start all the containers in detached mode"
 	${DOCKER_COMPOSE} up -d --build
 
 build:
-	@echo "$(_YELLOW)Build images$(_BACK)"
+	@echo "Build images"
 	$(DOCKER_COMPOSE) build
 
 start:
-	@echo "$(_YELLOW)Start containers$(_BACK)"
+	@echo "Start containers"
 	$(DOCKER_COMPOSE) start
 
 restart:
-	@echo "$(_YELLOW)Restart containers$(_BACK)"
+	@echo "Restart containers"
 	$(DOCKER_COMPOSE) restart
 
 stop:
-	@echo "$(_YELLOW)Stop containers$(_BACK)"
+	@echo "Stop containers"
 	$(DOCKER_COMPOSE) stop
 
 down:
-	@echo "$(_YELLOW)Stop and remove containers, volumes and networks$(_BACK)"
+	@echo "Stop and remove containers, volumes and networks"
 	$(DOCKER_COMPOSE) down --rmi all --volumes --remove-orphans
-# --rmi all = rm(remove) all i(images)
-
-containers:
-	@echo "$(_YELLOW)List running containers$(_BACK)"
-	$(DOCKER_COMPOSE) ps
-
-images:
-	@echo "$(_YELLOW)List images-$(_BACK)"
-	docker images
-
-volumes:
-	@echo "$(_YELLOW)List volumes$(_BACK)"
-	docker volume ls
 
 clean: down
 
 fclean: clean
-	@echo "$(_YELLOW)Removes images, containers and volumes$(_BACK)"
+	@echo "Removes images, containers and volumes"
 	sudo rm -rf /home/$(USER)/data/wordpress/*
 	sudo rm -rf /home/$(USER)/data/mysql/*
 
 prune: fclean
-	@echo "$(_YELLOW)Removes all unused images, containers, networks and volumes$(_BACK)"
+	@echo "Removes all unused images, containers, networks and volumes"
 	sudo docker system prune -f -a
-# -a = all
-# -f = force
 
 re: fclean all
 
-.PHONY: all build up start restart stop down containers images volumes clean fclean prune re
+.PHONY: all build up start restart stop down clean fclean prune re
--- srcs/requirements/wordpress/tools/launch_wordpress.sh
+++ srcs/requirements/wordpress/tools/launch_wordpress.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# static website
+# Move static website files
 mv /tmp/index.html /var/www/html/wordpress/index.html
 mv /tmp/style.css /var/www/html/wordpress/style.css
 mv /tmp/images /var/www/html/wordpress/images
@@ -8,32 +8,38 @@ mv /tmp/images /var/www/html/wordpress/images
 # Change directory to where the WordPress file will be downloaded
 cd /var/www/html/wordpress
 
+# Download WordPress core
 wp core download --path="/var/www/html/wordpress" --allow-root
 
+# Create WordPress configuration
 wp config create --path="/var/www/html/wordpress" --allow-root --dbname=$DB_DATABASE --dbuser=$DB_USER --dbpass=$DB_USER_PASSWORD --dbhost=$DB_HOST --dbprefix=wp_
 
+# Install WordPress
 wp core install --path="/var/www/html/wordpress" --allow-root --url=$DOMAIN_NAME --title="$WP_SITE_TITLE" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
 
+# Update all plugins
 wp plugin update --allow-root --all
 
+# Create a new user
 wp user create --path="/var/www/html/wordpress" --allow-root $WP_USER $WP_USER_EMAIL --user_pass="$WP_USER_PASSWORD"
 
+# Create uploads directory and set permissions
 mkdir -p /var/www/html/wordpress/wp-content/uploads
-
 chown www-data:www-data /var/www/html/wordpress/wp-content/uploads -R
 
-# ---------redis--------
-wp config set WP_REDIS_HOST redis --allow-root 
-  	wp config set WP_REDIS_PORT 6379 --raw --allow-root
- 	wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
-  	wp config set WP_REDIS_PASSWORD $REDIS_PASSWORD --allow-root
- 	wp config set WP_REDIS_CLIENT phpredis --allow-root
-	wp plugin install redis-cache --activate --allow-root
-    wp plugin update --all --allow-root
-	wp redis enable --allow-root
-# ----------
+# Configure Redis caching
+wp config set WP_REDIS_HOST redis --allow-root
+wp config set WP_REDIS_PORT 6379 --raw --allow-root
+wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
+wp config set WP_REDIS_PASSWORD $REDIS_PASSWORD --allow-root
+wp config set WP_REDIS_CLIENT phpredis --allow-root
+wp plugin install redis-cache --activate --allow-root
+wp plugin update --all --allow-root
+wp redis enable --allow-root
 
-# LAUNCH PHP-FPM
+# Launch PHP-FPM (FastCGI Process Manager)
+# PHP-FPM is a FastCGI implementation for PHP that enables handling 
+# of PHP requests by a separate pool of processes, improving performance 
+# and resource utilization.
 mkdir -p /run/php/
-# LAUNCH PHP-FPM
 php-fpm7.3 -F
